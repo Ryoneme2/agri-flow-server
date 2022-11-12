319e0c499d70fb8881e928399853e84e53a04175
Ryoneme2 <worknarair6@gmail.com>
2022-11-12 16:52:17 +0700

    setup prisma model

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 6969;
 if (!process.env.PORT) console.warn('>> PORT from env not found then use static port instated <<');
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost/:${PORT} : run on ${process.env.NODE_ENV} server`);
+  console.log(`Server started at http://localhost:${PORT} : run on ${process.env.NODE_ENV} server`);
 })
