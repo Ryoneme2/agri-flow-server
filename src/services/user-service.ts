dbf261da25aa77f6b9dc5e7c18a7303dd4a73de5
Ryoneme2 <worknarair6@gmail.com>
2022-11-12 17:54:15 +0700

    mock add use service

---
+++ src/services/user-service.ts
@@ -0,0 +1,30 @@
+import moment from 'moment';
+import * as P from '@prisma/client';
+import dotenv from 'dotenv';
+import storageClient from '@config/connectBucket';
+import { v4 } from 'uuid';
+
+// import { hashString, decodePassword } from '@util/DecryptEncryptString';
+// import uploadToBucket from '@helper/uploadToBucket';
+
+dotenv.config();
+
+const prisma = new P.PrismaClient();
+
+const _add = async ({ username, password, email }) => {
+  try {
+    // await prisma.users.create({
+    //   data: {
+    //     username
+    //   }
+    // })
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      msg: 'internal error add user to database'
+    }
+  } finally {
+    prisma.$disconnect()
+  }
+}
