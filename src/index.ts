0892f59f6d1fcaacdca12a5e33ef1810b9ed7724
Ryoneme2 <worknarair6@gmail.com>
2022-11-12 15:08:04 +0700

    implement server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import 'module-alias/register';
+import express, { Express } from "express";
+import { rateLimit } from 'express-rate-limit';
+import cors from "cors";
+dotenv.config()
+
+const app: Express = express()
+
+// express server config
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(rateLimit({
+  windowMs: 10000,
+  max: 200,
+  message: "Too many requests from this IP, please try again"
+}))
+
+app.get('/testGet', (_, res) => {
+  res.status(200).send('Get success')
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from './index'
+
+const PORT = process.env.PORT || 6969;
+
+if (!process.env.PORT) console.warn('>> PORT from env not found then use static port instated <<');
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost/:${PORT} : run on ${process.env.NODE_ENV} server`);
+})
