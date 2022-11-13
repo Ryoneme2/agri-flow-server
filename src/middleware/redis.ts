56fe21d47ab29f1d7f10846b73cccb35b797ffad
Ryoneme2 <worknarair6@gmail.com>
2022-11-13 20:24:25 +0700

    add redis

---
+++ src/middleware/redis.ts
@@ -0,0 +1,32 @@
+import { client, connectClient, quitClient } from '@config/redisConnect'
+import { Request, Response, NextFunction } from 'express'
+import { httpStatus } from '@config/http'
+
+const cacheCheck = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await connectClient()
+
+    const params = Object.values(req.params)
+
+    if (!params) return next()
+
+    const value = await Promise.all(params.map(v => client.get(v)))
+
+    if (value.every(v => v === null)) {
+      next()
+      return
+    }
+
+    res.status(httpStatus.ok).send({
+      cached: true,
+      data: value,
+      msg: 'success get data'
+    })
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await quitClient()
+  }
+}
+
+export default cacheCheck
