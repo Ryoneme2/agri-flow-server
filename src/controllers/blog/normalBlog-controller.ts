ac394a8ccf7b782e9f63a556440eb490e6a58ce0
Ryoneme2 <worknarair6@gmail.com>
2022-11-14 04:21:06 +0700

    get and post blog

--- src/controllers/blog/normalBlog-controller.ts
+++ src/controllers/blog/normalBlog-controller.ts
@@ -43,6 +43,39 @@ const newBlog = async (req: IGetUserAuthInfoRequest, res: Response) => {
   }
 }
 
+const getOneBlog = async (req: IGetUserAuthInfoRequest, res: Response) => {
+  try {
+
+    const { blogId } = req.params
+
+    if (isNaN(+blogId)) return res.status(httpStatus.badRequest).send({
+      msg: 'blog id is invalid'
+    })
+    if (!blogId) return res.status(httpStatus.badRequest).send({
+      msg: 'blog id is require'
+    })
+
+    if (!req.jwtObject) return res.send({
+      status: httpStatus.unauthorized,
+      data: null,
+      message: 'Access denied unauthorized.'
+    })
+
+    const userObjJWT = req.jwtObject as UserJwtPayload;
+
+    const blog = await blogService._getOne(+blogId)
+
+    if (!blog.success) return res.status(httpStatus.conflict).send(blog)
+
+    res.send(blog)
+
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.internalServerError)
+  }
+}
+
 export {
-  newBlog
+  newBlog,
+  getOneBlog
 }
