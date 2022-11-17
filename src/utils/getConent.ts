f0ab7cb1e9dbc6e5dc83e95335851abfb00bdf54
Ryoneme2 <worknarair6@gmail.com>
2022-11-17 20:05:01 +0700

    get content

--- src/utils/getConent.ts
+++ src/utils/getConent.ts
@@ -1,6 +1,9 @@
 const getContent = (ctx: string) => {
-  const paragraph = ctx?.split('<p>')[1] || ''
-  return paragraph?.split('</p>')[0] || ''
+  return ctx?.split('<p>')?.map((v, i) => {
+    if (i === 0 || v.startsWith('<img')) return
+    if (v.startsWith('<strong>')) return v.split('<strong>')[1].split('<strong>')[0]
+    return v.split('</p>')[0]
+  }).filter(v => v !== undefined).join('') || ''
 }
 
 export default getContent
