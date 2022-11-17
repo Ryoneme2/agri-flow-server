cbca57b490fd8a70b6b8b8df58b8b1482616069b
Ryoneme2 <worknarair6@gmail.com>
2022-11-17 18:48:06 +0700

    error return next on middleware

--- src/utils/getThumbnail.ts
+++ src/utils/getThumbnail.ts
@@ -1,5 +1,5 @@
 const getThumbnail = (c: string) => {
-  return c.split('src="').map(v => v.split('"')[0]).filter(v => v.startsWith('http') || v.startsWith("data:image"))
+  return c.split('<img src="').map(v => v.split('"')[0]).filter(v => v.startsWith('http') || v.startsWith("data:image"))
 }
 
 export default getThumbnail
